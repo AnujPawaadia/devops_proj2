d3c15b5b6af44ffc11738835648a86ebc756379c
Dhruv1069 <130770067+Dhruv1069@users.noreply.github.com>
2023-10-02 21:50:08 +0530

    background change

--- Final.py
+++ Final.py
@@ -63,6 +63,8 @@ root = tk.Tk()
 root.title("Password Generator")
 root.geometry("400x500+700+300")
 
+root['background']='#33a1c9'
+
 # Create widgets
 length_label = tk.Label(root, text="Password Length:",font=("Arial", 15))
 length_entry = tk.Entry(root,width=15)
